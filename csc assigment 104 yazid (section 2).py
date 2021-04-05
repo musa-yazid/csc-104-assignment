a07b22b21ad4e49b43723a133aa9b87d485bdd96
musa-yazid <81954492+musa-yazid@users.noreply.github.com>
2021-04-05 08:50:27 +0100

    Add files via upload

---
+++ csc assigment 104 yazid (section 2).py
@@ -0,0 +1,22 @@
+
+name="musa yazid erena"
+dept="science education"
+print(name,"\n",dept)
+scare="boo!"
+print(scare)
+n1=3
+n2=5
+n3=n1+n2
+print(n3)    
+rem=12/0.5
+print(rem)
+total=4+5
+print(total)
+rem=8%3
+print(rem)
+q = 2
+t =4
+p = q * t
+w = t+q
+x = p*w
+print(x)
